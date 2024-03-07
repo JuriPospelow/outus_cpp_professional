166189227fd61a9fa6f97f9d55520bfc9fb16bb8
Juri <jur@pospelow.de>
2024-03-07 10:53:01 +0100

    task1: init

---
+++ task1/main.cpp
@@ -0,0 +1,11 @@
+// #include "lib.h"
+
+#include <iostream>
+
+int main (int, char **)
+{
+    std::cout << "build " << "BUILD"/* version() */ << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
