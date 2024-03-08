af46ec0c881720255574ed0cade5a93401573928
Juri <jur@pospelow.de>
2024-03-08 18:28:40 +0100

    task1: use files git hub patch version

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,11 +1,13 @@
 #include "build/version.h"
+#include "lib.h"
 
 #include <iostream>
 
 int main (int, char **)
 {
 
-    std::cout << "build " << BUILD_VERSION << std::endl;
+    // std::cout << "build " << BUILD_VERSION << std::endl;
+    std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, World!" << std::endl;
 
     return 0;
