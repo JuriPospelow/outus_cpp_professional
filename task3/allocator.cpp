a9ac4ffcd0570d9d05da646ae4e6ba33404a77de
Juri <jur@pospelow.de>
2024-03-25 07:23:00 +0100

    task3: fill map with factorial

--- task3/allocator.cpp
+++ task3/allocator.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int factorial(int n) {
+  if(n > 1)
+    return n * factorial(n - 1);
+  else
+    return 1;
+}
+
 int main ()
 {
+    map <int,int> map1;
+
+    for(int i = 0; i < 10; ++i) {
+     map1.insert ( std::pair<int,int>(i,factorial(i)));
+    }
+
+    for(auto item : map1){
+        cout << item.first <<  " " << item.second << endl;
+    }
+
     return 0;
 }
