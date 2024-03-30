769a5ce09b1cf1cd8d817b9c44a7d28352eda124
Juri <jur@pospelow.de>
2024-03-30 08:41:43 +0100

    task4: init

---
+++ task4/print_ip.cpp
@@ -0,0 +1,155 @@
+#include <cstddef>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+namespace juri {
+
+template <typename T>
+vector<string> print_ip(T bytes) {
+	vector<string> tmp = {
+		to_string(((bytes >> 56) & 0xff)),
+		to_string(((bytes >> 48) & 0xff)),
+		to_string(((bytes >> 40) & 0xff)),
+		to_string(((bytes >> 32) & 0xff)),
+		to_string(((bytes >> 24) & 0xff)),
+		to_string(((bytes >> 16) & 0xff)),
+		to_string(((bytes >>  8) & 0xff)),
+		to_string(((bytes      ) & 0xff))
+	};
+	return tmp;
+}
+
+template <>
+vector<string> print_ip(string bytes) {
+	vector<string> tmp = {
+		bytes
+	};
+
+	return tmp;
+}
+
+template <>
+vector<string> print_ip(vector<int> bytes) {
+	vector<string> tmp;
+	tmp.reserve(bytes.size());
+	for(size_t i=0; i < bytes.size(); ++i){
+		tmp.push_back(to_string(bytes[i]));
+	}
+	return tmp;
+}
+
+template <>
+vector<string> print_ip(list<short> bytes) {
+	vector<string> tmp;
+	tmp.reserve(bytes.size());
+	for_each(bytes.cbegin(), bytes.cend(), [&] (const short t) {tmp.push_back(to_string(t));});
+	return tmp;
+}
+
+template <>
+vector<string> print_ip(int32_t bytes) {
+	vector<string> tmp = {
+		to_string(((bytes >> 24) & 0xff)),
+		to_string(((bytes >> 16) & 0xff)),
+		to_string(((bytes >>  8) & 0xff)),
+		to_string(((bytes      ) & 0xff))
+	};
+
+	return tmp;
+}
+
+template <>
+vector<string> print_ip(int16_t bytes) {
+	vector<string> tmp = {
+		to_string(((bytes >>  8) & 0xff)),
+		to_string(((bytes      ) & 0xff))
+	};
+
+	return tmp;
+}
+
+template <>
+vector<string> print_ip(int8_t bytes) {
+vector<string> tmp = {
+	to_string(((bytes      ) & 0xff))
+		};
+	return tmp;
+}
+
+}
+/*
+print_ip( int8_t{-1} ); // 255
+print_ip( int16_t{0} ); // 0.0
+print_ip( int32_t{2130706433} ); // 127.0.0.1
+print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41
+print_ip( std::string{“Hello, World!”} ); // Hello, World!
+print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
+print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100
+print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+*/
+
+int main() {
+
+	vector<string> tmp = juri::print_ip(int32_t{2130706433});
+	cout << "juri::print_ip(int32_t{2130706433}): ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	tmp = juri::print_ip(int16_t{0});
+	cout << "juri::print_ip(int16_t{0}): ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	tmp = juri::print_ip(int8_t{-1});
+	cout << "juri::print_ip(int8_t{-1}: ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	tmp = juri::print_ip(int64_t{8875824491850138409});
+	cout << "juri::print_ip(int64_t{8875824491850138409}: ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	// tmp = juri::print_ip("Hello, World!");
+	tmp = juri::print_ip(string{"Hello, World!"} );
+	cout << "juri::print_ip(string{“Hello, World!”}: ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	tmp = juri::print_ip(vector<int>{100, 200, 300, 400});
+	cout << "juri::print_ip(vector<int>{100, 200, 300, 400}: ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	tmp = juri::print_ip(list<short>{400, 300, 200, 100});
+	cout << "juri::print_ip(list<short>{400, 300, 200, 100}: ";
+	for(size_t i = 0; i < tmp.size();++i){
+		std::cout << tmp[i];
+		if(i != tmp.size()-1) cout << ".";
+	}
+	cout << endl;
+
+	return 0;
+}
