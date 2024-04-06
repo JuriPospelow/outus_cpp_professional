8447ce6964683e41ea4848c5402a27b67c7dca33
Juri Pospelow <juri@pospelow.de>
2024-04-06 16:17:42 +0200

    task2:gtest update gtest

--- task2/gtests/tests.cpp
+++ task2/gtests/tests.cpp
@@ -4,7 +4,7 @@
 
  TEST(CheckFiltration, ReverseLexicographical) {
     vector<string> tmp_v {"67.232.81.208", "85.254.10.197", "23.240.215.189", "1.29.168.152", "185.69.186.168"};
-    vector<vector<int>> expecteed = {{185,69,186,168}, {85,254,10,197}, {67,232,81,208}, {23,240,215,189}, {1,29,168,152}};
+    vector<IP_Addr> expecteed = {{185,69,186,168}, {85,254,10,197}, {67,232,81,208}, {23,240,215,189}, {1,29,168,152}};
 
     IP_Adr_Pool pool(tmp_v);
     int mi = 0;
