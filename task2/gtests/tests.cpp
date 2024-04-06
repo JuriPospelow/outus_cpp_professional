4b7586ef2ab25e8a134f70b3adf1289b450300ff
Juri Pospelow <juri@pospelow.de>
2024-04-06 10:19:08 +0200

    task2:gtest: update gtest

--- task2/gtests/tests.cpp
+++ task2/gtests/tests.cpp
@@ -4,14 +4,18 @@
 
  TEST(CheckFiltration, ReverseLexicographical) {
     vector<string> tmp_v {"67.232.81.208", "85.254.10.197", "23.240.215.189", "1.29.168.152", "185.69.186.168"};
-    vector<string> expecteed = {"185.69.186.168", "85.254.10.197", "67.232.81.208", "23.240.215.189", "1.29.168.152"};
+    vector<vector<int>> expecteed = {{185,69,186,168}, {85,254,10,197}, {67,232,81,208}, {23,240,215,189}, {1,29,168,152}};
 
     IP_Adr_Pool pool(tmp_v);
-    pool.reverse();
-    auto result = pool.get();
-
-    for(auto i = 0; i < expecteed.size(); ++i) {
-       ASSERT_EQ(expecteed[i], result[i]);
-    }
+    int mi = 0;
+    int vi = 0;
+   for (auto i(pool._ip_pool.rbegin()), end(pool._ip_pool.rend()); i != end; ++i) {
+      for(auto item = (*i).cbegin(); item < (*i).cend(); ++item) {
+         ASSERT_EQ(expecteed[mi][vi], *item);
+         ++vi;
+      }
+      ++mi;
+      vi = 0;
+   }
 
 }
